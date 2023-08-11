bd564a42f19500db4797a8cafadf7a6d89d39808
Mehmet OKUYAR <58508173+MehmetOKUYAR@users.noreply.github.com>
2023-08-11 20:28:10 +0300

    installations

---
+++ anydesk-installation.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install wget
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt update
+sudo apt install anydesk
+
---
+++ github-desktop-installition.sh
@@ -0,0 +1,3 @@
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.1.1-linux1/GitHubDesktop-linux-3.1.1-linux1.deb
+sudo apt-get install gdebi-core 
+sudo gdebi GitHubDesktop-linux-3.1.1-linux1.deb
